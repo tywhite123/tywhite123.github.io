6b4a17df0b693e37d43099011c01573149a28e5b
Tyler Whitehead <tylerwhitehead@Tylers-MacBook-Pro.local>
2024-07-08 21:43:42 +0100

    test 5

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Testing sonthing
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
